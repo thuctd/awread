53acbf3ccce540380854aa73d104c710f7c9d025
hiepxanh <hiepxanh@gmail.com>
2020-11-16 18:50:46 +0700

    added shell to writer-web

--- features/writer-web/shell/src/lib/writer-web-shell.module.ts
+++ features/writer-web/shell/src/lib/writer-web-shell.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ShellComponent } from './shell/shell.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [],
+  },
+];
 
 @NgModule({
-  imports: [CommonModule],
+  declarations: [ShellComponent],
+  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule,
+  ],
 })
 export class WriterWebShellModule {}
