e8d19b1b972a14dad9c0b11a7eba393fe4fdf7de
hiepxanh <hiepxanh@gmail.com>
2020-11-29 11:29:09 +0700

    clear all

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,5 @@ module.exports = {
   projects: [
     '<rootDir>/apps/api',
     '<rootDir>/libs/api-interfaces',
-    '<rootDir>/apps/writer/web',
-    '<rootDir>/apps/writer/mobile',
-    '<rootDir>/apps/reader/web',
-    '<rootDir>/apps/reader/mobile',
   ],
 };
