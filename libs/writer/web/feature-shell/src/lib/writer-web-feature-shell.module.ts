5205d874c73607d13fde8f6ea834844e0ec9fadc
hiepxanh <hiepxanh@gmail.com>
2020-12-03 20:11:36 +0700

    add font awesome example

--- libs/writer/web/feature-shell/src/lib/writer-web-feature-shell.module.ts
+++ libs/writer/web/feature-shell/src/lib/writer-web-feature-shell.module.ts
@@ -8,6 +8,8 @@ import {
   ShellMobileLayout,
   NotFoundPage,
 } from '@awread/writer/web/shared';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { WriterWebUiAuthModule } from '@awread/writer/web/ui-auth';
 import { WriterWebUiSingleModule } from '@awread/writer/web/ui-single';
 import { WriterWebUiCreationsModule } from '@awread/writer/web/ui-creations';
@@ -36,11 +38,21 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forRoot(routes),
     GlobalCoreModule,
+    FontAwesomeModule,
     WriterWebSharedModule,
     WriterWebUiAuthModule,
     WriterWebUiSingleModule,
     WriterWebUiCreationsModule,
   ],
-  exports: [RouterModule, GlobalCoreModule],
+  exports: [
+    RouterModule, 
+    GlobalCoreModule,
+    FontAwesomeModule
+  ],
 })
-export class WriterWebFeatureShellModule {}
+export class WriterWebFeatureShellModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    library.addIcons(faCoffee, faHeart);
+  }
+}
