d50ee8d9daafd95450600aa27e9ba01ff2573ddc
hiepxanh <hiepxanh@gmail.com>
2020-12-11 23:55:14 +0700

    webpack clear for angular

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,40 @@
+const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-knobs/register'],
+  webpackFinal: (config, { configType }) => {
+    const tailwindConfig = require("../tailwind.config.js")(configType === 'PRODUCTION');
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      loader: 'postcss-loader',
+      options: {
+        postcssOptions: {
+          ident: 'postcss',
+          syntax: 'postcss-scss',
+          plugins: [
+            require('postcss-import'),
+            require('tailwindcss')(tailwindConfig),
+            require('autoprefixer'),
+          ]
+        }
+      }
+    });
+    // config['stats'] = {
+    //   all: false
+    // }
+    config.plugins.push(
+      new FilterWarningsPlugin({
+        exclude: /postcss-import/
+      })
+    );
+
+    // Return the altered config
+    return config;
+  },
 };
