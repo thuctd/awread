5f89f87241c7d985c0f791e8cc4cd605ae560aa6
hiepxanh <hiepxanh@gmail.com>
2020-12-15 17:35:29 +0700

    added auth facade

--- libs/writer/web/feature-auth/src/lib/facades/auth.facade.ts
+++ libs/writer/web/feature-auth/src/lib/facades/auth.facade.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { LoginGear, LogoutGear, RegisterGear } from '../gears';
+import { CurrentUserQuery } from '../states/current-user';
 
 @Injectable({ providedIn: 'root' })
 export class AuthFacade {
+  currentUser$ = this.currentUserQuery.select();
+
+  get currentUser() {
+    return this.currentUserQuery.getValue();
+  }
 
   constructor(
     private logoutGear: LogoutGear,
     private loginGear: LoginGear,
     private registerGear: RegisterGear,
+    private currentUserQuery: CurrentUserQuery,
   ) {
   }
 
