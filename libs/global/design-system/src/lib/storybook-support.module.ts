b8acfd45e6efd174c570ce37f4e0c34d3c553c89
hiepxanh <hiepxanh@gmail.com>
2020-12-25 10:49:07 +0700

    storybook is good to go

--- libs/global/design-system/src/lib/storybook-support.module.ts
+++ libs/global/design-system/src/lib/storybook-support.module.ts
@@ -9,7 +9,7 @@ declare const __ISSTORYBOOK__: boolean;
 console.log('is storybook?', __ISSTORYBOOK__);
 
 @NgModule({
-  imports: [CommonModule, __ISSTORYBOOK__ ? RouterModule.forRoot([]) : RouterModule],
+  imports: [CommonModule, __ISSTORYBOOK__ ? RouterModule.forRoot([], { useHash: true }) : RouterModule],
   exports: [RouterModule, ReactiveFormsModule, FontAwesomeModule],
   declarations: [],
   providers: [__ISSTORYBOOK__ ? { provide: APP_BASE_HREF, useValue: '/' } : []],
