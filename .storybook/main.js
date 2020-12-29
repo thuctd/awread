78930ace3725703615eeb3383ec92ece273c8d70
hiepxanh <hiepxanh@gmail.com>
2020-12-29 10:44:03 +0700

    update webpack, build faster

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,27 +18,9 @@ module.exports = {
   // },
 
   webpackFinal: (config, { configType }) => {
-    const tailwindConfig = require("../tailwind.config.js")(configType === 'PRODUCTION');
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-      test: /\.scss$/,
-      loader: 'postcss-loader',
-      options: {
-        postcssOptions: {
-          ident: 'postcss',
-          syntax: 'postcss-scss',
-          plugins: [
-            require('postcss-import'),
-            require('tailwindcss')(tailwindConfig),
-            require('autoprefixer'),
-          ]
-        }
-      }
-    });
+    const isProd = config.mode === "production";
+    const tailwindConfig = require("../tailwind.config.js")(isProd);
+    patchPostCSS(config, tailwindConfig, true);
 
     config.plugins.push(
       new FilterWarningsPlugin({
@@ -59,3 +41,35 @@ module.exports = {
 
   },
 };
+
+
+function patchPostCSS(webpackConfig, tailwindConfig, components = false) {
+  if (!tailwindConfig) {
+    console.error('Missing tailwind config :', tailwindConfig);
+    return;
+  }
+  const pluginName = "autoprefixer";
+  for (const rule of webpackConfig.module.rules) {
+    if (!(rule.use && rule.use.length > 0) || (!components && rule.exclude)) {
+      continue;
+    }
+    for (const useLoader of rule.use) {
+      if (!(useLoader.options && useLoader.options.postcssOptions)) {
+        continue;
+      }
+      const originPostcssOptions = useLoader.options.postcssOptions;
+      useLoader.options.postcssOptions = (loader) => {
+        const _postcssOptions = originPostcssOptions(loader);
+        const insertIndex = _postcssOptions.plugins.findIndex(
+          ({ postcssPlugin }) => postcssPlugin && postcssPlugin.toLowerCase() === pluginName
+        );
+        if (insertIndex !== -1) {
+          _postcssOptions.plugins.splice(insertIndex, 0, ["tailwindcss", tailwindConfig]);
+        } else {
+          console.error(`${pluginName} not found in postcss plugins`);
+        }
+        return _postcssOptions;
+      };
+    }
+  }
+}
