9bdc4fea26f5f7b09ca7151fae641dc6a7a803dc
hiepxanh <hiepxanh@gmail.com>
2021-01-02 19:01:41 +0700

    added powerspowers

---
+++ libs/global/powers/README.md
@@ -0,0 +1,7 @@
+# global-powers
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test global-powers` to execute the unit tests.
---
+++ tools/generators/power/index.ts
@@ -0,0 +1,42 @@
+import { chain, Rule, SchematicContext, Tree, schematic, externalSchematic } from '@angular-devkit/schematics';
+import { componentSetting } from '../../utility/edit-angular-json';
+import { exportToLibIndex } from '../../utility/export-to-index';
+import { guessProjectToSchema } from '../../utility/guess-workspace';
+
+export default function (schema: any): Rule {
+    return async (tree: Tree, context: SchematicContext) => {
+        schema.project = 'global-powers';
+        schema = await guessProjectToSchema(tree, schema, context);
+        return async (tree: Tree, context: SchematicContext) => {
+            const actions = schema.list.split(',').map(name => singleAction(schema, context, name))
+            return chain([
+                ...actions
+            ])
+        }
+    }
+}
+
+function singleAction(schema, context, name) {
+    return async (tree) => {
+        schema.name = name;
+        schema = await guessProjectToSchema(tree, schema, context);
+        return chain([
+            externalSchematic('@schematics/angular', 'module', {
+                name,
+                project: schema.project,
+            }),
+            externalSchematic('@schematics/angular', 'component', {
+                ...componentSetting,
+                name,
+                module: name,
+                project: schema.project,
+                export: true,
+                inlineStyle: schema.inline,
+                inlineTemplate: schema.inline,
+                skipTests: true,
+                viewEncapsulation: 'None'
+            }),
+            exportToLibIndex(schema.projectType, schema.projectRoot, `export * from './lib/${name}/${name}.module'`),
+        ])
+    }
+}
