404d66ac9da286626bff79ffea93c08fde619439
hiepxanh <hiepxanh@gmail.com>
2021-01-15 11:00:19 +0700

    build: allow nginx proxy backend

--- apps/reader/web/src/app/app.module.ts
+++ apps/reader/web/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReaderWebFeatureShellModule } from '@awread/reader/web/feature-shell';
-
+import { GlobalCoreModule } from '@awread/global/core';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReaderWebFeatureShellModule],
+  imports: [BrowserModule, ReaderWebFeatureShellModule, GlobalCoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
