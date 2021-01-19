fbc63a886dc779fae39daa2b2f016259f155be64
hiepxanh <hiepxanh@gmail.com>
2021-01-20 06:05:25 +0700

    build: update allo cros origin

--- apps/backend/graphql/src/main.ts
+++ apps/backend/graphql/src/main.ts
@@ -53,7 +53,9 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('allow', 'GET,PUT,POST,DELETE');
+  // NOTE: guard down
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
