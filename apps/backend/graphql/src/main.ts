908ca9e663399fe970d2c88c1d5aa4b0d667a794
hiepxanh <hiepxanh@gmail.com>
2021-01-20 05:56:49 +0700

    build: change allow header

--- apps/backend/graphql/src/main.ts
+++ apps/backend/graphql/src/main.ts
@@ -53,7 +53,7 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('allow', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
