14728b4ba75170b6ddbad52f0e9e20705923c4d6
thanhtung060201 <thanhtung060201@gmail.com>
2021-02-04 20:29:27 +0700

    done #100

---
+++ libs/reader/web/ui-auth/src/lib/atomics/molecs/register-form/register-form.molec.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faLock, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'molec-register-form',
+  templateUrl: './register-form.molec.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterFormMolec implements OnInit {
+  @Input() type = 'password';
+  icons = { faLock, faEnvelope, faUser };
+  @Input() form: FormGroup = this.fb.group({
+    displayName: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    confirmPassword: ['', [Validators.required]],
+  });
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+}
