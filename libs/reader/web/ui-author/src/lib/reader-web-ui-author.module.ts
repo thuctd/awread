97fa2f842ce757183297b6f9bc020b1ca6ec6948
Le Ngoc Quy <lengocquy0122@gmail.com>
2021-03-06 13:45:48 +0700

    fix navigate

--- libs/reader/web/ui-author/src/lib/reader-web-ui-author.module.ts
+++ libs/reader/web/ui-author/src/lib/reader-web-ui-author.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReaderWebUiAuthorRoutingModule } from './reader-web-ui-author-routing.module';
-import { AuthorLayout } from './layouts/author/author.layout';
 
 @NgModule({
-  imports: [CommonModule, ReaderWebUiAuthorRoutingModule],
-  declarations: [AuthorLayout],
-  exports: [AuthorLayout],
+  imports: [CommonModule],
 })
 export class ReaderWebUiAuthorModule {}
