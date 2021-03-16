46d299bd08d1bd0bb264287d5372601714c143a9
Le Ngoc Quy <lengocquy0122@gmail.com>
2021-03-16 22:02:57 +0700

    fix review chi Thuong

--- libs/reader/web/ui-market/src/lib/atomics/molecs/search-form/search-form.molec.ts
+++ libs/reader/web/ui-market/src/lib/atomics/molecs/search-form/search-form.molec.ts
@@ -18,13 +18,30 @@ export class SearchFormMolec implements OnInit {
   @Output() searchEvent = new EventEmitter();
   @Input() selectSearch = [{
     title: 'Thể Loại',
-    widthClass: 'w-96'
+    widthClass: 'w-96',
+    options: [
+      { id: '1', name: 'Lãng mạn' },
+      { id: '2', name: 'Kiếm hiệp' },
+      { id: '3', name: 'Trinh thám' },
+      { id: '4', name: 'Kinh dị' },
+      { id: '5', name: 'Xuyên không' }
+    ]
   }, {
     title: 'Tình Trạng',
-    widthClass: 'w-96'
+    widthClass: 'w-96',
+    options: [
+      { id: '1', name: 'Đã hoàn thành' },
+      { id: '2', name: 'Chưa hoàn thành' }
+    ]
   }, {
     title: 'Năm phát hành',
-    widthClass: 'w-60'
+    widthClass: 'w-60',
+    options: [
+      { id: '1', name: '2000' },
+      { id: '2', name: '2001' },
+      { id: '3', name: '2002' },
+      { id: '4', name: '2003' },
+      { id: '5', name: '2004' }]
   }];
 
   constructor() { }
