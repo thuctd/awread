493160fa62a494c1449ace3243bb312d596e9f96
Le Ngoc Quy <lengocquy0122@gmail.com>
2021-03-18 18:04:12 +0700

    not default home

--- libs/reader/web/ui-author/src/lib/reader-web-ui-author-routing.module.ts
+++ libs/reader/web/ui-author/src/lib/reader-web-ui-author-routing.module.ts
@@ -5,10 +5,9 @@ import { AuthorLayout } from './layouts/author/author.layout';
 
 declare const window: Window & { haveMobile: boolean };
 
-// TODO: 2 trường hợp sẽ xảy ra nếu thõa mãn điều kiện load mặc đình vd: localhost:4200 => sẽ ra được trang home nhưng // lại bị ảnh hưởng khi load ở routing-module ở ui-author??? Ảnh em đính kém ở comment git...
 const routes: Routes = [
   {
-    path: '',
+    path: 'author',
     component: window.innerWidth <= 768 && window?.haveMobile ? SharedMobileLayout : SharedDesktopLayout,
     children: [
       {
@@ -16,14 +15,14 @@ const routes: Routes = [
         component: AuthorLayout,
         children: [
           {
-            path: ':author/{authorId}',
+            path: ':authorId',
             loadChildren: () =>
               window.innerWidth <= 768 && window?.haveMobile
                 ? import('./pages/author-mobile/author-mobile.module').then((m) => m.AuthorMobileModule)
                 : import('./pages/author-desktop/author-desktop.module').then((m) => m.AuthorDesktopModule),
           },
           {
-            path: ':{authorId}/books/:{bookId}',
+            path: ':authorId/books/:bookId',
             loadChildren: () =>
               window.innerWidth <= 768 && window?.haveMobile
                 ? import('./pages/author-book-mobile/author-book-mobile.module').then((m) => m.AuthorBookMobileModule)
@@ -39,4 +38,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class ReaderWebUiAuthorRoutingModule {}
+export class ReaderWebUiAuthorRoutingModule { }
