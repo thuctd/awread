8604725f5549d18ecf29290339eb501a53c32afc
thanhtung060201 <thanhtung060201@gmail.com>
2021-03-23 09:36:27 +0700

    fix introduction-shape-slogan

--- libs/reader/web/ui-market/src/lib/atomics/molecs/rd-introduction-shape-slogan/rd-introduction-shape-slogan.molec.ts
+++ libs/reader/web/ui-market/src/lib/atomics/molecs/rd-introduction-shape-slogan/rd-introduction-shape-slogan.molec.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'molec-rd-introduction-shape-slogan',
@@ -7,6 +7,15 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RdIntroductionShapeSloganMolec implements OnInit {
+  @Input() imageVertical = {
+    srcImg: 'https://kinhbacweb.com/wp-content/uploads/2018/11/tong-hop-nhung-slogan-hay-ve-kinh-doanh-ban-nen-biet-8375.jpg',
+    alt: '',
+  };
+
+  @Input() imageHorizontal = {
+    srcImg: 'https://meocuchay.com/wp-content/uploads/2020/08/slogan-hay.jpg',
+    alt: '',
+  };
   constructor() {}
 
   ngOnInit(): void {}
--- libs/reader/web/ui-market/src/lib/atomics/organs/rd-introduction-white-box-first/rd-introduction-white-box-first.organ.ts
+++ libs/reader/web/ui-market/src/lib/atomics/organs/rd-introduction-white-box-first/rd-introduction-white-box-first.organ.ts
@@ -3,20 +3,10 @@ import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core
 @Component({
   selector: 'organ-rd-introduction-white-box-first',
   templateUrl: './rd-introduction-white-box-first.organ.html',
-  styleUrls: ['./rd-introduction-white-box-first.organ.scss'],
+  styles: [``],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RdIntroductionWhiteBoxFirstOrgan implements OnInit {
-  @Input() imageVertical = {
-    srcImg: '/global-assets/images/smailImg.webp',
-    alt: '',
-  };
-
-  @Input() imageHorizontal = {
-    srcImg: 'https://via.placeholder.com/260x370.png',
-    alt: '',
-  };
-
   constructor() {}
 
   ngOnInit(): void {}
