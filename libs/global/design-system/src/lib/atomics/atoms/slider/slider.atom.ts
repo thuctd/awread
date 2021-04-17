35118d65579a96b58c78d71e14ef5398de4f6b48
thanhtung060201 <thanhtung060201@gmail.com>
2021-04-17 09:09:41 +0700

    fix height slider page home (desktop)

--- libs/global/design-system/src/lib/atomics/atoms/slider/slider.atom.ts
+++ libs/global/design-system/src/lib/atomics/atoms/slider/slider.atom.ts
@@ -16,7 +16,7 @@ export class SliderAtom implements OnInit {
   @Input() showArrow = 'true';
   @Input() imageSize = {
     width: '100%',
-    height: window.innerWidth > 1280 ? '600px' : window.innerWidth > 1024 ? '500px' : window.innerWidth > 768 ? '425' : '200px',
+    height: window.innerWidth > 1024 ? '500px' : window.innerWidth > 768 ? '425' : '200px',
     space: 1,
   };
   @Input() imageObject = [
@@ -24,23 +24,23 @@ export class SliderAtom implements OnInit {
       image: 'IMG 1',
       thumbImage: 'https://cohet.vn/upload/data/images/do-hoa/background/anh-nen-anime-dep-2.jpg',
       alt: 'alt of image',
-      id: '1'
+      id: '1',
     },
     {
       image: 'IMG 2',
       thumbImage: 'https://cohet.vn/upload/data/images/do-hoa/background/anh-nen-anime-dep-2.jpg',
       alt: 'Image alt',
-      id: '2'
+      id: '2',
     },
     {
       image: 'IMG 3',
       thumbImage: 'https://cohet.vn/upload/data/images/do-hoa/background/anh-nen-anime-dep-2.jpg',
       alt: 'Image alt',
-      id: '3'
+      id: '3',
     },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
