b44e196ee7e0772e69d314d08c326b3d9a05cdbb
hiepxanh <hiepxanh@gmail.com>
2021-05-12 08:34:37 +0700

    update: fix guard

--- libs/writer/web/ui-single/src/lib/guards/landing.guard.ts
+++ libs/writer/web/ui-single/src/lib/guards/landing.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
@@ -8,14 +9,10 @@ export class LandingGuard implements CanActivate {
   constructor(private afAuth: AngularFireAuth, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.afAuth.authState.pipe(
-      map((user) => {
-        if (user && user.uid) {
-          this.router.navigate(['list']);
-          return false;
-        }
-        return true;
-      })
-    );
+    if (localStorage.getItem('accessToken')) {
+      this.router.navigate(['list']);
+      return of(false);
+    }
+    return of(true);
   }
 }
