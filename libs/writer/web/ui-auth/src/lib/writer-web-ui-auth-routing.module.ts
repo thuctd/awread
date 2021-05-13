0cdcce245819fe9fcb2a6400664f70012d0780fe
hiepxanh <hiepxanh@gmail.com>
2021-05-13 08:39:03 +0700

    add: environment

--- libs/writer/web/ui-auth/src/lib/writer-web-ui-auth-routing.module.ts
+++ libs/writer/web/ui-auth/src/lib/writer-web-ui-auth-routing.module.ts
@@ -8,21 +8,21 @@ declare const window: Window & { haveMobile: boolean };
 
 const routes: Routes = [
   {
+    path: 'register-complete',
+    loadChildren: () =>
+      window.innerWidth <= 768 && window?.haveMobile
+        ? import(
+          './pages/register-complete-mobile/register-complete-mobile.module'
+        ).then((m) => m.RegisterCompleteMobileModule)
+        : import(
+          './pages/register-complete-desktop/register-complete-desktop.module'
+        ).then((m) => m.RegisterCompleteDesktopModule),
+  },
+  {
     path: '',
     component: AuthLayout,
     canActivate: [LandingGuard],
     children: [
-      {
-        path: 'register-complete',
-        loadChildren: () =>
-          window.innerWidth <= 768 && window?.haveMobile
-            ? import(
-              './pages/register-complete-mobile/register-complete-mobile.module'
-            ).then((m) => m.RegisterCompleteMobileModule)
-            : import(
-              './pages/register-complete-desktop/register-complete-desktop.module'
-            ).then((m) => m.RegisterCompleteDesktopModule),
-      },
       {
         path: 'new-password',
         loadChildren: () =>
