d985ded6d1ccb07ab4b663d286940c2cb114746d
thanhtung060201 <thanhtung060201@gmail.com>
2021-05-21 13:48:46 +0700

    update: newLogic

--- libs/core/news/src/lib/apis/news.api.ts
+++ libs/core/news/src/lib/apis/news.api.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Apollo, gql } from "apollo-angular";
+import { Apollo, gql } from 'apollo-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class NewsApi {
-
-  constructor(
-    private apollo: Apollo
-  ) { }
+  constructor(private http: HttpClient, private apollo: Apollo) {}
 
   get() {
+    return this.http.get(`https://raw.githubusercontent.com/small-cosmos/awread-hq/c569f648e1771b5c09a787e0f54c6c3115c84510/blogs/index.json`);
   }
-
 }
--- libs/core/news/src/lib/gears/news.gear.ts
+++ libs/core/news/src/lib/gears/news.gear.ts
@@ -1,13 +1,16 @@
+import { NewsStore } from './../states/news/news.store';
+import { NewsApi } from './../apis/news.api';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class NewsGear {
-
-  constructor(
-  ) {
-  }
+  constructor(private newsApi: NewsApi, private newsStore: NewsStore) {}
 
   get() {
+    return this.newsApi.get().pipe(
+      tap((value) => console.log('news', value)),
+      tap((value: any) => this.newsStore.set(value))
+    );
   }
-
 }
