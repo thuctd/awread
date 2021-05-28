fbac4b73c2497314b0ef7a755c35bae5f4cf7fcd
hiepxanh <hiepxanh@gmail.com>
2021-05-28 17:01:23 +0700

    update: avatar not using setting

--- libs/core/setting/src/lib/facades/setting.facade.ts
+++ libs/core/setting/src/lib/facades/setting.facade.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 import { SettingGear } from '../gears';
+import { SettingQuery } from '../states/setting';
 
 @Injectable({ providedIn: 'root' })
 export class SettingFacade {
-
+  setting$ = this.settingQuery.select();
+  storage$ = this.settingQuery.select(state => state.storage);
   constructor(
     private settingGear: SettingGear,
+    private settingQuery: SettingQuery,
   ) {
   }
 
   get() {
     return this.settingGear.get();
+  }
+
+  getStorage() {
+    return this.settingQuery.getValue().storage;
   }
 
 }
--- libs/core/setting/src/lib/states/setting/setting.store.ts
+++ libs/core/setting/src/lib/states/setting/setting.store.ts
@@ -8,13 +8,13 @@ export interface SettingState {
 
 export function createInitialState(): SettingState {
   return {
-    storage: '',
-    version: ''
+    storage: 'https://awread-bucket.ss-hn-1.bizflycloud.vn',
+    version: '1.0.0'
   };
 }
 
 @Injectable({ providedIn: 'root' })
-@StoreConfig({ name: 'setting', resettable: true })
+@StoreConfig({ name: 'setting', resettable: true, idKey: 'settingId' })
 export class SettingStore extends Store<SettingState> {
 
   constructor() {
