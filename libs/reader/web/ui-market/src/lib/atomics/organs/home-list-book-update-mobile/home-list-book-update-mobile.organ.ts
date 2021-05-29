a59a68283c8efcb7106b799d3b3645d1acbf718d
thanhtung060201 <thanhtung060201@gmail.com>
2021-05-29 13:46:18 +0700

    xoa owl-carousel home-mobile

--- libs/reader/web/ui-market/src/lib/atomics/organs/home-list-book-update-mobile/home-list-book-update-mobile.organ.ts
+++ libs/reader/web/ui-market/src/lib/atomics/organs/home-list-book-update-mobile/home-list-book-update-mobile.organ.ts
@@ -4,14 +4,18 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'organ-home-list-book-update-mobile',
   templateUrl: './home-list-book-update-mobile.organ.html',
-  styleUrls: ['./home-list-book-update-mobile.organ.scss'],
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeListBookUpdateMobileOrgan implements OnInit {
   @Input() counter = Array;
   @Input() titlePage = 'Truyện mới cập nhật';
-  prevIcon = `<img src="/global-assets/images/arrow_left.webp" alt="arrow_left">`;
-  nextIcon = `<img src="/global-assets/images/arrow_right.webp" alt="arrow_right">`;
   @Input() srcImg = '/global-assets/images/image.webp';
   @Input() altImg = 'Placeholder';
   @Input() books = [];
@@ -29,32 +33,6 @@ export class HomeListBookUpdateMobileOrgan implements OnInit {
     },
   };
   constructor() {}
-
-  @Input() customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 500,
-    nav: true,
-    autoWidth: false,
-    navText: [this.prevIcon, this.nextIcon],
-    responsive: {
-      0: {
-        items: 3,
-      },
-      320: {
-        items: 3,
-      },
-      640: {
-        items: 4,
-      },
-      728: {
-        items: 5,
-      },
-    },
-  };
 
   ngOnInit(): void {}
 }
