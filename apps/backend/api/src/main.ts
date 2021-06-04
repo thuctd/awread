ff9d3138c04f90f6a1d1f150ec54f0ef9fd16cfa
hiepxanh <hiepxanh@gmail.com>
2021-06-04 10:44:52 +0700

    update: typeof

--- apps/backend/api/src/main.ts
+++ apps/backend/api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   console.log('environment, buildenvironment', environment, buildEnvironment);
   console.log('NODE_ENV', process.env.NODE_ENV, buildEnvironment.NODE_ENV);
   console.log('ENVIRONMENT', process.env.ENVIRONMENT);
-  console.log('WTF why NODE_ENV??', process.env);
+  console.log('WTF why NODE_ENV??', process.env, typeof process.env);
 
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
