51177eb3b86f6a6254873de5fccc170ae52a0b40
hiepxanh <hiepxanh@gmail.com>
2021-06-08 11:03:12 +0700

    fix: reset password global

--- libs/writer/web/ui-auth/src/lib/pages/forgot/forgot.page.ts
+++ libs/writer/web/ui-auth/src/lib/pages/forgot/forgot.page.ts
@@ -1,6 +1,11 @@
 import { Router } from '@angular/router';
 import { ChangeDetectorRef, Directive, Injectable, OnInit } from '@angular/core';
-import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { ForgotPasswordFacade } from '@awread/core/users';
 import { SnackbarService } from '@awread/global/packages';
 @Injectable({
@@ -14,26 +19,28 @@ export class ForgotPage implements OnInit {
     private router: Router,
     private forgotPasswordFacade: ForgotPasswordFacade,
     private snackbarService: SnackbarService,
-    private cd: ChangeDetectorRef,
-  ) { }
+    private cd: ChangeDetectorRef
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
   forgotSubmitEvent(email: string) {
     console.log('email: ', email);
     this.forgotPasswordFacade.send(email).subscribe((result: any) => {
-      if (result.success) {
+      if (result.success == true) {
         this.isSentEmail = true;
         this.cd.detectChanges();
       } else {
-        this.snackbarService.showWarning(result.message);
+        const message = result.message.includes('not found')
+          ? 'Địa chỉ email này không có trong hệ thống'
+          : result.message;
+        this.snackbarService.showWarning(message);
       }
       console.log('result', result);
-    })
+    });
     // this.forgotPasswordFacade.sendLinkResetPassword(email);
   }
   closeEvent() {
     console.log('send link reset password ok!');
     this.router.navigate(['/login']);
   }
-
 }
