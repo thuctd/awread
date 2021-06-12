7bc95a66918191f9ae5dff3448df8946e4eef305
hiepxanh <hiepxanh@gmail.com>
2021-06-12 16:43:22 +0700

    update: database currentUser to thisUser and refesh_token

--- libs/core/users/src/lib/apis/current-user.api.ts
+++ libs/core/users/src/lib/apis/current-user.api.ts
@@ -8,10 +8,7 @@ import { map } from 'rxjs/operators';
 export class CurrentUserApi {
   // user$: Observable<any>;
   // user: any;
-  constructor(
-    private apollo: Apollo,
-    private currentUserQuery: CurrentUserQuery,
-  ) {
+  constructor(private apollo: Apollo, private currentUserQuery: CurrentUserQuery) {
     // setInterval(function () {
     //   this.httpClient
     //     .get(`https://raw.githubusercontent.com/small-cosmos/awread-hq/master/news/index.json`)
@@ -20,18 +17,108 @@ export class CurrentUserApi {
     //       result = result.banner;
     //     });
     //  }, 3000);
-
-
   }
 
-
   getCurrentUser() {
-    return this.apollo.query({
-      query: gql`
-        mutation currentUser {
-          currentUser(input: {}) {
+    return this.apollo
+      .query({
+        query: gql`
+          mutation thisUser {
+            thisUser(input: {}) {
+              user {
+                userId
+                username
+                email
+                phone
+                name
+                firstname
+                middlename
+                lastname
+                age
+                avatar
+                dob
+                gender
+                bio
+                websiteAddress
+                facebookAddress
+                updatedAt
+              }
+            }
+          }
+        `,
+      })
+      .pipe(map((res) => res.data?.['thisUser']?.['user']));
+  }
+
+  updateName(user) {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation updateUserByUserId($userId: UUID!, $firstname: String, $middlename: String, $lastname: String) {
+          updateUserByUserId(
+            input: {
+              userId: $userId
+              userPatch: { firstname: $firstname, middlename: $middlename, lastname: $lastname }
+            }
+          ) {
             user {
-              userId
+              firstname
+              middlename
+              lastname
+            }
+          }
+        }
+      `,
+      variables: {
+        ...user,
+        userId: this.currentUserQuery.getUserId(),
+      },
+    });
+  }
+
+  update(user) {
+    return this.apollo.mutate({
+      mutation: gql`
+        mutation updateUserByUserId(
+          $userId: UUID!
+          $age: BigFloat
+          $gender: BigFloat
+          $username: String
+          $email: String
+          $phone: String
+          $name: String
+          $firstname: String
+          $middlename: String
+          $lastname: String
+          $avatar: Boolean
+          $dob: String
+          $bio: String
+          $websiteAddress: String
+          $facebookAddress: String
+        ) {
+          updateUserByUserId(
+            input: {
+              userId: $userId
+              userPatch: {
+                age: $age
+                gender: $gender
+                username: $username
+                email: $email
+                phone: $phone
+                name: $name
+                firstname: $firstname
+                middlename: $middlename
+                lastname: $lastname
+                avatar: $avatar
+                dob: $dob
+                bio: $bio
+                websiteAddress: $websiteAddress
+                facebookAddress: $facebookAddress
+              }
+            }
+          ) {
+            user {
+              age
+              gender
               username
               email
               phone
@@ -39,124 +126,21 @@ export class CurrentUserApi {
               firstname
               middlename
               lastname
-              age
               avatar
               dob
-              gender
               bio
               websiteAddress
               facebookAddress
-              updatedAt
             }
           }
         }
-      `,
-    }).pipe(
-      map(res => res.data?.['currentUser']?.['user'])
-    )
-  }
-
-  updateName(user) {
-    return this.apollo.mutate({
-      mutation: gql`
-      mutation updateUserByUserId(
-        $userId: UUID!,
-        $firstname: String,
-        $middlename: String,
-        $lastname: String
-        ) {
-        updateUserByUserId(
-          input: {
-            userId: $userId,
-            userPatch: {
-              firstname: $firstname,
-              middlename: $middlename,
-              lastname: $lastname,
-            }
-          }
-        ) {
-          user {
-            firstname
-            middlename
-            lastname
-          }
-        }
-      }
       `,
       variables: {
         ...user,
-        userId: this.currentUserQuery.getUserId()
+        userId: this.currentUserQuery.getUserId(),
       },
-    })
+    });
   }
-
-  update(user) {
-    return this.apollo.mutate({
-      mutation: gql`
-      mutation updateUserByUserId(
-        $userId: UUID!,
-        $age: BigFloat,
-        $gender: BigFloat,
-        $username: String,
-        $email: String,
-        $phone: String,
-        $name: String,
-        $firstname: String,
-        $middlename: String,
-        $lastname: String,
-        $avatar: Boolean,
-        $dob: String,
-        $bio: String,
-        $websiteAddress: String,
-        $facebookAddress: String,
-        ) {
-        updateUserByUserId(
-          input: {
-            userId: $userId,
-            userPatch: {
-              age: $age,
-              gender: $gender,
-              username: $username,
-              email: $email,
-              phone: $phone,
-              name: $name,
-              firstname: $firstname,
-              middlename: $middlename,
-              lastname: $lastname,
-              avatar: $avatar,
-              dob: $dob,
-              bio: $bio,
-              websiteAddress: $websiteAddress,
-              facebookAddress: $facebookAddress,
-            }
-          }
-        ) {
-          user {
-            age
-            gender
-            username
-            email
-            phone
-            name
-            firstname
-            middlename
-            lastname
-            avatar
-            dob
-            bio
-            websiteAddress
-            facebookAddress
-          }
-        }
-      }
-      `,
-      variables: {
-        ...user,
-        userId: this.currentUserQuery.getUserId()
-      },
-    })
-  }
-
 
   linkSocial(credential) {
     return this.apollo.mutate({
@@ -178,8 +162,8 @@ export class CurrentUserApi {
       `,
       variables: {
         ...credential,
-        userId: this.currentUserQuery.getUserId()
+        userId: this.currentUserQuery.getUserId(),
       },
-    })
+    });
   }
 }
